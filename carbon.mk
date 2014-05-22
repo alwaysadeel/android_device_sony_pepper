36a261ea619dab0358578cd26364971da68b70ed
Adeel Javed <alwaysadeel@gmail.com>
2014-05-22 09:29:55 +0000

    Create carbon.mk

---
+++ carbon.mk
@@ -0,0 +1,17 @@
+$(call inherit-product, device/sony/pepper/full_pepper.mk)
+
+# Inherit Carbon common GSM stuff.
+$(call inherit-product, vendor/carbon/config/common_gsm.mk)
+
+# Inherit Carbon common Phone stuff.
+$(call inherit-product, vendor/carbon/config/common_phone.mk)
+
+# Enhanced NFC
+$(call inherit-product, vendor/carbon/config/nfc_enhanced.mk)
+ 
+# Boot Animation
+TARGET_SCREEN_HEIGHT := 854
+TARGET_SCREEN_WIDTH := 480
+
+PRODUCT_NAME := carbon_pepper
+PRODUCT_DEVICE := pepper
