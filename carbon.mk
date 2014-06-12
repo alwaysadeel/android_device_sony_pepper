5f34b133140e37f190b01889c38c0a75f640aef9
Adeel Javed <alwaysadeel@gmail.com>
2014-06-12 08:09:53 +0500

    delete gsm

--- carbon.mk
+++ carbon.mk
@@ -1,8 +1,5 @@
 $(call inherit-product, device/sony/pepper/full_pepper.mk)
 
-# Inherit Carbon common GSM stuff.
-$(call inherit-product, vendor/carbon/config/common_gsm.mk)
-
 # Inherit Carbon common Phone stuff.
 $(call inherit-product, vendor/carbon/config/common_phone.mk)
  
